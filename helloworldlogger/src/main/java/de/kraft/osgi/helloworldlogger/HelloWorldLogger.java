ec4bce22d7b48e6a74c385a9715cfa79015ab5d0
Alex1902 <kraftation@gmail.com>
2015-11-22 22:45:22 +0100

    comment removed.

--- helloworldlogger/src/main/java/de/kraft/osgi/helloworldlogger/HelloWorldLogger.java
+++ helloworldlogger/src/main/java/de/kraft/osgi/helloworldlogger/HelloWorldLogger.java
@@ -2,11 +2,8 @@ package de.kraft.osgi.helloworldlogger;
 
 public class HelloWorldLogger implements HelloWorldLoggerService
 {
-//    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger( HelloWorldLogger.class );
-
     public void debug( final String msg )
     {
         System.out.println( msg );
-//        LOG.debug( msg );
     }
 }
